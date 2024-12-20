085a61b0dcb95c19f3058ccac54d3c8bba13636d
André Kaique <112278911+AndrekaiqueDevjunior@users.noreply.github.com>
2024-12-20 13:52:11 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('portfolio.html')  # Certifique-se de que o arquivo portfolio.html está na pasta templates
 
 @app.route('/about')
 def about():
     return 'About'
+
+if __name__ == '__main__':
+    app.run(debug=True)
